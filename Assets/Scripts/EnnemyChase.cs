b858070b2ac45a0c9519bae8522c3c7af8062602
svtozq <rpenea@esiee-it>
2025-02-26 18:49:35 +0100

    add enemy chase and health damage

---
+++ Assets/Scripts/EnnemyChase.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyChase : MonoBehaviour
+{
+	public GameObject Player;
+	public GameObject PointA;
+	public GameObject PointB;
+
+	private Rigidbody2D rb2d;
+    private Animator anim;
+	private Transform currentPoint;
+	private float distance; 
+    private float move;
+	public float moveSpeed;
+
+	public int damage;
+	private float lastAttackTime = 0f;
+	public float attackCooldown = 1f;
+	private PlayerHealth playerHealth;
+
+    // Awake is called before the 
+
+	void Awake()
+    {	
+		playerHealth = Player.GetComponent<PlayerHealth>();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+		currentPoint = PointB.transform;
+		anim.SetInteger("Dir", 0);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		if (Vector2.Distance(transform.position, Player.transform.position) > 6)
+		{	
+			anim.SetInteger("Dir", 0);
+			Vector2 Point = currentPoint.position - transform.position;
+			if (currentPoint == PointB.transform)
+			{
+				rb2d.velocity = new Vector2(3, 0);
+			}
+			else 
+			{
+				rb2d.velocity = new Vector2(-3, 0);
+			}
+
+			if (Vector2.Distance(transform.position, currentPoint.position) < 1.2f  && currentPoint == PointB.transform)
+			{
+				currentPoint = PointA.transform;				
+			}
+
+			if (Vector2.Distance(transform.position, currentPoint.position) < 1.2f  && currentPoint == PointA.transform)
+			{	
+				currentPoint = PointB.transform;	
+			}
+		}
+
+		else if (Vector2.Distance(transform.position, Player.transform.position) <= 6)
+		{	
+			anim.SetInteger("Dir", 1);
+			transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, moveSpeed * Time.deltaTime);
+			
+			if (Vector2.Distance(transform.position, Player.transform.position) <= 2f)
+			{	
+				if (Time.time >= lastAttackTime + attackCooldown)
+				{
+					playerHealth.TakeDamage(damage);
+					lastAttackTime = Time.time;
+					Debug.Log("Enemy hit the player!");
+				}
+			}
+		}
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,6 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +10,7 @@ public class PlayerHealth : MonoBehaviour
     private int maxHealth = 100;
     private int currentHealth;
 
-    [SerializeField]private int damage;
+    private int damage;
     
     [SerializeField]private HealthBar healthBar;
     
@@ -25,9 +28,20 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+		if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+	
+	public void Die()
+    {
+        Debug.Log(gameObject.name + " has died!");
+        Destroy(gameObject);
+		UnityEditor.EditorApplication.isPlaying = false;
     }
 }
