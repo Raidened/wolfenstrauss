8292bf1552d8e62f88e1a6ac025931de43dab706
svtozq <rpenea@esiee-it>
2025-02-27 10:08:09 +0100

    creating tutorial map

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class MainMenu : MonoBehaviour
     public void Play()
     {
         Debug.Log("Play Game");
+		SceneManager.LoadScene("Tutorial");
     }
 
     public void Quit()
