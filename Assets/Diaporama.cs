c304418b97cccf00bb3939a078024ef729566580
Jean-Killian <killian.goulet.dubasque@gmail.com>
2025-02-28 02:36:50 +0100

    intro and end scene
    il faut resize les images jsp comment faire

---
+++ Assets/Diaporama.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class Diaporama : MonoBehaviour
+{
+    public GameObject[] slides; // Tableau des images
+    private int currentSlide = 0;
+
+    void Start()
+    {
+        UpdateSlides();
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown) // Change d'image à chaque touche pressée
+        {
+            currentSlide = (currentSlide + 1);
+            UpdateSlides();
+        }
+    }
+
+    void UpdateSlides()
+    {
+        for (int i = 0; i < slides.Length; i++)
+        {
+            slides[i].SetActive(i == currentSlide);
+        }
+    }
+}
